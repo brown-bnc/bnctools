cfb1912b182e2a7cdac541ab1525342f776f0bf4
Isabel Restrepo <isabelrestre@gmail.com>
2019-05-24 10:51:43 -0400

    mode export to file and adjust tests

---
+++ src/bnctools/orthanc_export.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+[options.entry_points] section in setup.cfg:
+
+    console_scripts =
+         fibonacci = bnctools.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+
+import argparse
+import sys
+import logging
+import time
+import os
+import os.path
+import sys
+import re
+
+from bnctools import __version__
+
+__author__ = "Isabel Restrepo"
+__copyright__ = "CCV - Brown University"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def normalize_path(s):
+    s = re.sub('[^a-zA-Z0-9-/-: ]', '_', s)
+    s = s.replace(' ', '_')
+    return s.lower()
+    
+def export_stable_study(orthanc, study_id, outdir):
+    
+    #retrieve study info
+    study_json = orthanc.get_study(study_id)
+    study_dicom_tags = study_json["MainDicomTags"]
+
+    if not study_json["IsStable"]:
+        print("Study NOT stable - retry later")
+        return
+
+    labname = study_dicom_tags["ReferringPhysicianName"]
+
+    if labname == "":
+        print("No Physician tag - Study is orphan")
+        labname = "orphan"    
+
+    series = study_json["Series"]
+                                
+    study_path =  normalize_path(outdir + '/' + labname + '/' + study_dicom_tags["StudyDate"] + '_' + study_dicom_tags["StudyDescription"])
+
+    print("--------------------------------------------")
+    print("Study Path " + study_path)
+    print("--------------------------------------------")
+
+
+    if not os.path.exists(study_path):
+        os.makedirs(study_path)
+
+    for s in series:
+
+        print("--------------------------------------------")
+        print("Processing DICOM Series " + s)
+
+        series_json = orthanc.get_one_series(s)
+        series_dicom_tags = series_json["MainDicomTags"]
+        instances = series_json["Instances"]
+
+
+        for i in instances[0:1]:
+            instance_tags = orthanc.get_instance_simplified_tags(instances[0])
+
+            dicom_path = normalize_path(study_path + '/' + "part-" + instance_tags["PatientName"] 
+                                                   + '/' + "ses-" + series_dicom_tags["SeriesDescription"])
+            
+            if not os.path.exists(dicom_path):
+                os.makedirs(dicom_path)
+
+            print(".", end ="")
+
+
+            dicom = orthanc.get_instance_file(i)
+            # Write to the file
+            f = open(dicom_path  + '/' + i + '.dcm', 'wb')
+            for chunk in dicom:
+                f.write(chunk)
+            f.close()
+
+        print("--------------------------------------------")
+
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Export DICOMs in Orthanc Study to BIDS-ready directory structure ")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='bnctools {ver}'.format(ver=__version__))
+    parser.add_argument(
+        dest="url",
+        help="url of Orthanc server",
+        type=str,
+        metavar="STR")
+    parser.add_argument(
+        dest="user",
+        help="User for Orthanc server",
+        type=str,
+        metavar="STR")
+    parser.add_argument(
+        dest="psswd",
+        help="Password for Orthanc server",
+        type=str,
+        metavar="STR")
+    parser.add_argument(
+        dest="out",
+        help="Parent output directory",
+        type=str,
+        metavar="STR")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting crazy calculations...")
+    print("Coming soon")
+    _logger.info("Script ends here")
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
--- tests/test_orthanc.py
+++ tests/test_orthanc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
+# import pytest
 import os
-from bnctools.orthanc import OrthancServer
-from pathlib import Path  
-from dotenv import load_dotenv
+import shutil
+import glob
+from bnctools.orthanc_export import export_stable_study
 from requests.auth import HTTPBasicAuth
 from orthanc_rest_client import Orthanc
 
@@ -14,65 +14,26 @@ __author__ = "Isabel Restrepo"
 __copyright__ = "Isabel Restrepo"
 __license__ = "mit"
 
-# #load .env file
-# env_path = Path('../../') / '.env'
-# load_dotenv(dotenv_path=env_path)
-auth = HTTPBasicAuth("orthanc", "orthanc")
-out_dir = "~/deleteme"
-orthanc = Orthanc("http://localhost:8042/", auth=auth)
-study_id = "d3fc1b48-73ba3acd-30b5ff85-ea27fa65-e3550bf4"
-study_json = orthanc.get_study(study_id)
-study_dicom_tags = study_json["MainDicomTags"]
 
-if not study_json["IsStable"]
-    print("Study NOT stable - retry later")
-    return
+def test_export_stable_study():
+    """Test exporting from Orthanc
+    This tests assumes an instace of Orthanc running on http://localhost:8042/
+    """
 
-labname = main_dicom_tags["ReferringPhysicianName"]
+    study_id = "d3fc1b48-73ba3acd-30b5ff85-ea27fa65-e3550bf4"
+    auth = HTTPBasicAuth("orthanc", "orthanc")
+    outdir = "deleteme"
+    orthanc = Orthanc("http://localhost:8042/", auth=auth)
 
-if labname == "":
-    print("Study is orphan")
-    labname = "orphan"    
+    if os.path.exists(outdir):
+        shutil.rmtree(outdir)
 
-series = study_json["Series"]
-                             
-study_path =  out_dir + '/' + study_dicom_tags["StudyDate"] + study_dicom_tags["StudyDescription"]
+    export_stable_study(orthanc, study_id, outdir)
 
-if not os.path.exists(study_path):
-    os.makedirs(study_path)
+    files = glob.glob(outdir + "**/**/*.dcm", recursive=True)
 
-for s in series:
+    print (len(files))
 
-    print("--------------------------------------------")
-    print("Processing DICOM Series " + s)
-    print("--------------------------------------------")
+    # assert
 
-    series_tags = orthanc.get_one_series(s)
-    instances = series_tags["Instances"]
-
-    for i in instances:
-        instance_tags = orthanc.get_instance_simplified_tags(instances[0])
-
-        dicom_path = study_path + '/' + "sub-" + instance_tags["PatientName"] 
-                                + '/' + "ses-" + series_tags["SeriesDescription"]
-        
-        if not os.path.exists(study_path):
-            os.makedirs(study_path)
-
-        # dicom = orthanc.get_instance_file(i)
-        # # Write to the file
-        # f = open(series_path  + '/' + '.dcm', 'wb'))
-        # f.write(dicom)
-        # f.close()
-
-
-    
-
-{'ID': 'd3fc1b48-73ba3acd-30b5ff85-ea27fa65-e3550bf4', 'IsStable': True, 'LastUpdate': '20190501T172956', 'MainDicomTags': {'AccessionNumber': '', 'InstitutionName': 'BROWN UNIVERSITY MRF', 'ReferringPhysicianName': '', 'StudyDate': '20190328', 'StudyDescription': 'MRF^Daily QA', 'StudyID': '1', 'StudyInstanceUID': '1.3.12.2.1107.5.2.43.67050.30000019031314111234400000184', 'StudyTime': '122934.099000'}, 'ParentPatient': '4bf75716-f1e6f4a6-a5a582d4-c5748d78-7b8f9edb', 'PatientMainDicomTags': {'PatientBirthDate': '19500101', 'PatientID': 'JACKHAMMER_QA', 'PatientName': 'JACKHAMMER_QA', 'PatientSex': 'O'}, 'Series': ['b5931257-e7295320-77429a04-841dafbc-6bf8037d', '499edd1f-7ad20a64-04512efe-3f50f204-9fdfe117', 'e4c92ea8-95adb53d-3d005dd2-13e76ffb-b0b38757', '904f6941-96dfd3f6-3ca9997b-79015997-b2a3fd86', 'f809cce9-f9647b03-8b218be7-42fe82b0-768733f6'], 'Type': 'Study'}
-
-# def test_orthanc_connection():
-#     assert fib(1) == 1
-#     assert fib(2) == 1
-#     assert fib(7) == 13
-#     with pytest.raises(AssertionError):
-#         fib(-10)
+test_export_stable_study()
